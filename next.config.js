7ec59d941c930c28da3150f1a721707fa87d6ebd
Kanan Asadov <kenan@tenktek.com>
2022-08-17 11:57:09 +0300

    fix: Bundle analyzer is always enabled

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-// const withPlugins = require("next-compose-plugins");
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+  enabled: true,
 })
 
 const nextConfig = {
@@ -11,7 +8,7 @@ const nextConfig = {
   performance: {
     hints: 'warning',
   },
-  // assetPrefix: isHeroku ? "" : isProd ? 'https://static.teklifimgelsin.info/' : '',
+
   generateBuildId: async () => {
     // You can, for example, get the latest git commit hash here
     return '16-08-2022'
@@ -30,43 +27,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
 
-  webpack: (config, {isServer}) => {
-    console.log(isServer)
-    if (!isServer && isProd) {
-
-      // config.optimization.splitChunks.cacheGroups.rcComponents = {
-      //   test: /[\\/]node_modules[\\/](rc-field-form|rc-motion|async-validator)[\\/]/,
-      //   name: "rcComponents",
-      //   priority: 0,
-      //   reuseExistingChunk: true,
-      // }
-
-      // config.optimization.splitChunks.cacheGroups.tempRC = {
-      //   test: /[\\/]node_modules[\\/](async-validator)[\\/]/,
-      //   name: "tempRC",
-      //   priority: 50,
-      //   reuseExistingChunk: true,
-      // }
-      //
-      // config.optimization.splitChunks.cacheGroups.utilityVendor = {
-      //   test: /[\\/]node_modules[\\/](lodash|moment|moment-timezone)[\\/]/,
-      //   name: "utilityVendor"
-      // }
-      //
-      // config.optimization.splitChunks.cacheGroups.devOverlay = {
-      //   test: /[\\/]node_modules[\\/]next[\\/]dist[\\/]compiled[\\/]@next[\\/](react-dev-overlay)[\\/]/,
-      //   name: "devOverlay",
-      //   chunks: "all",
-      //   priority: -20
-      // }
-      //
-      // config.optimization.splitChunks.cacheGroups.queueAnimAndTween = {
-      //   test: /[\\/]node_modules[\\/](rc-queue-anim|tween-functions)[\\/]/,
-      //   name: "queueAnimAndTween",
-      // }
-    }
-    return config;
-  },
 }
 /** @type {import('next').NextConfig} */
 module.exports = (_phase, { defaultConfig }) => {
